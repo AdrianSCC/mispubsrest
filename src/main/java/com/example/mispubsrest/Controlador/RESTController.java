8a42f6f2e0a2d37b44178c74f8434085ae02ca11
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-21 11:10:23 +0100

    Insert en el controlador

--- src/main/java/com/example/mispubsrest/Controlador/RESTController.java
+++ src/main/java/com/example/mispubsrest/Controlador/RESTController.java
@@ -2,7 +2,11 @@ package com.example.mispubsrest.Controlador;
 
 
 import com.example.mispubsrest.Dao.PubDao;
+import com.example.mispubsrest.Modelos.Pub;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +21,21 @@ public class RESTController {
     @RequestMapping(value = "hola", method = RequestMethod.GET)
     public String hola(){
         return "Hola, todo ok";
+    }
 
+    /**
+     *              CRUD PARA LOS PUBS
+     * ========================================
+     */
+
+
+    /**
+     * Método para insertar un pub
+     */
+    @RequestMapping(value="pubs",method=RequestMethod.POST)
+    public ResponseEntity<Pub> insertarPub(@RequestBody Pub pub){
+        Pub p = daoPub.save(pub);
+        return ResponseEntity.ok(p);
     }
 
 }
