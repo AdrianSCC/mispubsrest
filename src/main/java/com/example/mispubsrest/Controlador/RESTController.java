b042a909d740dfc7efa359623aa3282ad321dd99
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-21 10:50:27 +0100

    Creando Controlador

---
+++ src/main/java/com/example/mispubsrest/Controlador/RESTController.java
@@ -0,0 +1,22 @@
+package com.example.mispubsrest.Controlador;
+
+
+import com.example.mispubsrest.Dao.PubDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RESTController {
+
+    @Autowired
+    private PubDao daoPub;
+
+    @RequestMapping(value = "hola", method = RequestMethod.GET)
+    public String hola(){
+        return "Hola todo ok";
+    }
+
+}
