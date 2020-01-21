2b58334d0c75b183d45cbe818be92aca0cec4674
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-21 09:36:55 +0100

    Initial commit

---
+++ src/main/java/com/example/mispubsrest/MispubsrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mispubsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MispubsrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MispubsrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mispubsrest/MispubsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mispubsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MispubsrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
